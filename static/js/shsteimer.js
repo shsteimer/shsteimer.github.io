404c8bf85dffe28a293b62ed169890af56a6b4f2
Sean Steimer <ssteimer@crownpartners.com>
2014-01-23 19:44:52 -0600

    too many changes to keep track up, basically just a bunch of updates

--- static/js/shsteimer.js
+++ static/js/shsteimer.js
@@ -1,3 +1,24 @@
-shsteimer = {};
+shsteimer = {
+
+	init: function(){
+
+		!(function($) {
+  			$.each(window._q,function(index,f){
+  				f();
+  			});
+  		})(shsteimer.$);
+	},
+
+	archives: {
+		init: function() {
+			var href = location.href;
+	    	var split = href.split("#");
+	    	var elem = shsteimer.$('#collapse-' + split[1]);
+	    	if(elem && elem.hasClass('panel-collapse')) {
+	    		elem.collapse('show');
+	    	}
+		}
+	}
+};
 
 shsteimer.$ = jQuery.noConflict();
