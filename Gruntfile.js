6a7adc7a1429d35c35990c2aec107c3f6b0dace2
Sean Steimer <ssteimer@crownpartners.com>
2014-01-28 01:02:02 -0800

    add clean & copy to grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,19 +2,24 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        clean: [ 'static' ],
         recess: {
             dev: {
                 options: {
                     compile: true
                 },
-                files: {'static/css/shsteimer.css': ['_static/less/shsteimer.less']}
-    		    },
-    		    dist: {
+                files: {
+                    'static/css/shsteimer.css': ['_static/less/shsteimer.less']
+                }
+    	    },
+    		dist: {
                 options: {
                     compress: true
                 },
-                files: {'static/css/shsteimer.min.css': ['_static/less/shsteimer.less']}
-    		    }
+                files: {
+                    'static/css/shsteimer.min.css': ['_static/less/shsteimer.less']
+                }
+    	    }
         },
         uglify: {
             dist: {
@@ -22,16 +27,44 @@ module.exports = function(grunt) {
                     mangle: true
                 },
                 files: {
-                    'static/js/shsteimer.min.js': ['static/js/shsteimer.js']
+                    'static/js/shsteimer.min.js': ['_static/js/shsteimer.js']
+                }
+            }
+        },
+        copy: {
+            exec: {
+                files: [
+                    {expand: true, cwd: '_static/', src: ['img/*'], dest: 'static/'},
+                    {expand: true, cwd: '_static/', src: ['js/*.js'], dest: 'static/'},
+                ]    
+            }
+        },
+        jekyll: {  
+            dev: {
+                options: {
+                    drafts: true,
+                    config: '_config.yml,_config-dev.yml',
+                    serve: true
+                }
+            },
+            dist: {
+                options: {
+                    safe: true,
+                    serve: true
                 }
             }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-recess');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-jekyll');
 
-    // Default task(s).
-    grunt.registerTask('default', ['recess','uglify']);
+    // register tasks
+    grunt.registerTask('default', ['clean','recess','uglify','copy']);
+    grunt.registerTask('serve-dev', ['clean','recess','uglify','copy','jekyll:dev']);
+    grunt.registerTask('serve-prod', ['clean','recess','uglify','copy','jekyll:dist']);
 };
