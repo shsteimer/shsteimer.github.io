a51db51507c499d64e5105c5bb8ce7d08c1e54b6
Sean Steimer <ssteimer@crownpartners.com>
2014-01-27 21:35:55 -0800

    add npm and recess to bootstrap script so that we can custom compile bootstrap less to css

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,6 @@
+sudo apt-get update
 sudo apt-get -y install build-essential
 sudo /opt/vagrant_ruby/bin/gem install jekyll rdiscount --no-ri --no-rdoc
+sudo apt-get -y install nodejs npm
+sudo npm config set registry http://registry.npmjs.org/
+sudo npm install recess -g
